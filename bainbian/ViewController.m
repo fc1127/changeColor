cbae25348d6f8c220ea9cceced76ceda5b37c39b
自由自在 <1462384560@qq.com>
2017-06-26 10:03:35 +0800

    渐变色

--- bainbian/ViewController.m
+++ bainbian/ViewController.m
@@ -17,6 +17,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+//    gradientLayer.colors = @[(__bridge id)[UIColor redColor].CGColor, (__bridge id)[UIColor whiteColor].CGColor, (__bridge id)[UIColor grayColor].CGColor];
+////    gradientLayer.locations = @[@0, @0, @0.3];
+//    gradientLayer.startPoint = CGPointMake(0, 0);
+//    gradientLayer.endPoint = CGPointMake(0, 1.0);
+//    gradientLayer.frame = CGRectMake(0, 100, 300, 300);
+//    [self.view.layer addSublayer:gradientLayer];
+
+    UIView *view = [[UIView alloc] initWithFrame:self.view.bounds];
+    
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    
+    //设置开始和结束位置(设置渐变的方向)
+    
+    gradient.startPoint = CGPointMake(0, 1.0);
+    
+    gradient.endPoint = CGPointMake(0, 0);
+    
+    gradient.frame =CGRectMake(100,100,400,400);
+    
+    gradient.colors = [NSArray arrayWithObjects:(id)[UIColor colorWithWhite:0.8 alpha:0.8].CGColor,(id)[UIColor whiteColor].CGColor,nil];
+    
+    [view.layer insertSublayer:gradient atIndex:0];
+    
+    [self.view addSubview:view];
 }
 
 
